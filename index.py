51b8a7250f488a30e1127c4325b9d5bfba1e5156
Dean987 <dean861031@gmail.com>
2022-11-04 23:12:50 +0800

    make it better

--- index.py
+++ index.py
@@ -1,3 +1,9 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 from flask import Flask,render_template
 app = Flask(__name__)
 
@@ -7,6 +13,7 @@ def index():
     homepage += "<a href=/I>關於俊祥</a><br>"
     homepage += "<a href=/L>應徵的公司資訊</a><br>"
     homepage += "<a href=/O>履歷</a><br>"
+    homepage += "<br><a href=/read>讀取Firestore資料</a><br>"
     return homepage
 
 
@@ -21,6 +28,17 @@ def company():
 @app.route("/O")
 def test():
  return render_template("test.html") 
+
+
+@app.route("/read")
+def read():
+    Result = ""     
+    collection_ref = db.collection("靜宜資管")    
+    docs = collection_ref.order_by("mail", direction=firestore.Query.DESCENDING).get()    
+    for doc in docs:         
+        Result += "文件內容：{}".format(doc.to_dict()) + "<br>"    
+    return Result
+
 
 #if __name__ == "__main__":
     #app.run()
